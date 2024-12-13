e256755d49485ecf61136b695d91e266fbc4eb79
A. Fox <anthonyfox1988@gmail.com>
2024-12-13 10:27:06 -0800

    feat(highlights): add snacks.dashboard

--- lua/jellybeans/groups/snacks.lua
+++ lua/jellybeans/groups/snacks.lua
@@ -7,6 +7,18 @@ function M.get(c, opts)
     SnacksNormal = { bg = c.bg },
     SnacksNormalNC = { bg = c.bg },
     SnacksBackdrop = { bg = c.bg },
+
+    SnacksDashboardNormal = { bg = c.bg },
+    SnacksDashboardDesc = { bg = c.bg, fg = c.biloba_flower },
+    SnacksDashboardFile = { bg = c.bg, fg = c.koromiko },
+    SnacksDashboardDir = { bg = c.bg, fg = c.perano },
+    SnacksDashboardFooter = { bg = c.bg, fg = c.biloba_flower },
+    SnacksDashboardHeader = { bg = c.bg, fg = c.perano },
+    SnacksDashboardIcon = { bg = c.bg, fg = c.raw_sienna },
+    SnacksDashboardKey = { bg = c.bg, fg = c.wewak },
+    SnacksDashboardTerminal = { bg = c.bg },
+    SnacksDashboardSpecial = { bg = c.bg, fg = c.goldenrod },
+    SnacksDashboardTitle = { bg = c.bg, fg = c.perano },
   }
 end
 
