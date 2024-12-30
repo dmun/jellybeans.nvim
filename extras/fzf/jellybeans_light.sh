888b3d459a2483b4b387c8cef6021b01a106d1e9
A. Fox <anthonyfox1988@gmail.com>
2024-12-30 12:44:35 -0800

    feat(extras): Add light theme for fzf and wezterm

---
+++ extras/fzf/jellybeans_light.sh
@@ -0,0 +1,7 @@
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#e0dcd7,bg:#fdf6e3,spinner:#954d3b,hl:#234291 \
+--color=fg:#262626,header:#876820,info:#234291,pointer:#954d3b \
+--color=marker:#904070,fg+:#6a4abd,prompt:#234291,hl+:#234291 \
+--color=border:#fdf6e3 \
+--multi"
+
