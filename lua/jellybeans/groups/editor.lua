a2a38b87726189d887847841256b4ea82f0f999f
A. Fox <anthonyfox1988@gmail.com>
2025-02-07 07:33:44 -0800

    fix(highlights): diagnostic underline highlights

--- lua/jellybeans/groups/editor.lua
+++ lua/jellybeans/groups/editor.lua
@@ -64,15 +64,15 @@ function M.get(c, opts)
     GitSignsDeleteVirtLnInLine = c.git.delete,
 
     DiagnosticError = { fg = c.diag.error },
-    DiagnosticUnderlineError = { undercurl = true, fg = c.diag.error },
+    DiagnosticUnderlineError = { undercurl = true, sp = c.diag.error },
     DiagnosticWarn = { fg = c.diag.warning },
-    DiagnosticUnderlineWarn = { undercurl = true, fg = c.diag.warning },
+    DiagnosticUnderlineWarn = { undercurl = true, sp = c.diag.warning },
     DiagnosticInfo = { fg = c.diag.info },
-    DiagnosticUnderlineInfo = { undercurl = true, fg = c.diag.info },
+    DiagnosticUnderlineInfo = { undercurl = true, sp = c.diag.info },
     DiagnosticHint = { fg = c.diag.hint },
-    DiagnosticUnderlineHint = { undercurl = true, fg = c.diag.hint },
+    DiagnosticUnderlineHint = { undercurl = true, sp = c.diag.hint },
     DiagnosticOk = { fg = c.diag.ok },
-    DiagnosticUnderlineOk = { undercurl = true, fg = c.diag.ok },
+    DiagnosticUnderlineOk = { undercurl = true, sp = c.diag.ok },
   }
 end
 
