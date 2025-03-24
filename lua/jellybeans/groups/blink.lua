b67cf9dec4e1b4fbcb10ddfb3459e91fc6d8d038
David van Munster <davidru01@live.nl>
2025-03-24 22:40:45 +0100

    yes

--- lua/jellybeans/groups/blink.lua
+++ lua/jellybeans/groups/blink.lua
@@ -2,9 +2,16 @@ local Util = require("jellybeans.util")
 
 local M = {}
 
+---@param c Palette
+---@param opts jellybeans.Config
 function M.get(c, opts)
   return {
     BlinkCmpLabelDetail = { fg = Util.blend(Util.fg, 0.6, Util.bg) },
+    BlinkCmpSignatureHelp = { bg = c.grey_one },
+    BlinkCmpSignatureHelpBorder = { bg = c.grey_one },
+    BlinkCmpDoc = { bg = c.grey_one },
+    BlinkCmpDocBorder = { bg = c.grey_one },
+    BlinkCmpDocSeparator = { fg = c.grey, bg = c.grey_one },
   }
 end
 
